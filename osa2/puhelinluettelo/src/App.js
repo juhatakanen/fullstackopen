7ba38ddbe9e0ec1544ab74d567e8784de5b0cf47
Juha Takanen <juhatakanen@hotmail.com>
2022-08-31 12:35:14 +0300

    2.6 tehty

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (e) => {
+    console.log(e.target.value);
+    setNewName(e.target.value)
+  }
+
+  const addPerson = (e) => {
+    e.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => 
+        <p key={person.name}>
+          {person.name}
+        </p>
+        )}
+    </div>
+  )
+
+}
+
+export default App
