1cf366bd39915d9a9ba410bb8d44de70ee194e44
Juha Takanen <juhatakanen@hotmail.com>
2022-09-05 14:04:32 +0300

    2.15 tehty

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -41,9 +41,14 @@ const App = () => {
         number: newNumber
       }
       
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+      axios
+      .post('http://localhost:3001/persons', personObject)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      })
+  
     }
   }
 
